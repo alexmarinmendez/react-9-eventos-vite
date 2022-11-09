603650e571618b92b6a97773d2e5b4bae27a6aec
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-09 09:07:46 -0500

    feat: add event listener in Functional Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import {EventsClass} from './components/EventsClass';
-// import EventsFunctional from './components/EventsFunctional';
+// import {EventsClass} from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
 
 const App = () => {
   return (
     <>
       <h1>Hola Mundo!!!</h1>
       <hr />
-      <EventsClass />
+      <EventsFunctional />
     </>
   )
 }
--- src/components/EventsFunctional.jsx
+++ src/components/EventsFunctional.jsx
@@ -3,9 +3,21 @@ import { useState } from "react";
 const EventsFunctional = () => {
     const [counter, setCounter] = useState(0);
 
+    const sumar = () => {
+        setCounter(counter + 1)
+    }
+
+    const restar = () => {
+        setCounter(counter - 1)
+    }
+
     return(
         <>
             <h2>Soy EventsFunctional</h2>
+            <nav>
+                <button onClick={sumar}>+</button>
+                <button onClick={restar}>-</button>
+            </nav>
             <span>{counter}</span>
         </>
     )
