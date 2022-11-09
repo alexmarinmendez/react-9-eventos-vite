e63569b852d6264dc5a50313541e435e23ccfb0c
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-09 09:02:31 -0500

    feat: add event listener part 2 (binding)

--- src/components/EventsClass.jsx
+++ src/components/EventsClass.jsx
@@ -2,13 +2,17 @@ import { Component } from "react";
 
 export class EventsClass extends Component {
     constructor() {
+        super()
         this.state = {
             counter: 0
         }
+        this.sumar = this.sumar.bind(this);
     }
 
     sumar() {
-        console.log('Estoy sumando...')
+        this.setState({
+            counter: this.state.counter + 1
+        })
     }
 
     render() {
